097752cb7d4b8b1e4e0c34ffc3ab641ba74d15fc
Michael <misha.sunsetboy@gmail.com>
2019-11-29 01:11:54 +0300

    дополнены тесты, зачатки документации

--- tests/GeoClientTest.php
+++ tests/GeoClientTest.php
@@ -1,6 +1,7 @@
 <?php
 
 
+use GeoServiceClient\exceptions\UnauthorizedException;
 use GeoServiceClient\GeoClient;
 use GuzzleHttp\Client;
 use GuzzleHttp\Psr7\Response;
@@ -20,15 +21,55 @@ class GeoClientTest extends TestCase
             "lng" => "37.073220",
             "isCapital" => 0
         ]);
-        $httpClientMock = $this->createMock(Client::class);
-        $httpClientResponseMock = $this->createMock(Response::class);
-        $httpClientResponseMock->method('getBody')->willReturn($jsonResponse);
-        $geoClient = new GeoClient('http://test.local');
-        $geoClient->setHttpClient($httpClientMock);
+        $httpStatusCode = 200;
 
-        $httpClientMock->method('request')->willReturn($httpClientResponseMock);
+        $geoClient = $this->getGeoClientWithMockedHttpClient($jsonResponse, $httpStatusCode);
+
         $town = $geoClient->getTownById(33);
 
         $this->assertEquals("Апрелевка", $town->getName());
+
+        // имитация ответа со статусом 401
+        $httpStatusCode = 401;
+        $geoClient = $this->getGeoClientWithMockedHttpClient($jsonResponse, $httpStatusCode);
+        $this->expectException(UnauthorizedException::class);
+        $geoClient->getTownById(33);
+    }
+
+    public function testGetRegionById()
+    {
+        $jsonResponse = json_encode([
+            "id" => 33,
+            "name" => "Липецкая область",
+            "countryId" => 2,
+            "alias" => "lipetskaya",
+        ]);
+        $httpStatusCode = 200;
+
+        $geoClient = $this->getGeoClientWithMockedHttpClient($jsonResponse, $httpStatusCode);
+
+        $town = $geoClient->getRegionById(33);
+
+        $this->assertEquals("Липецкая область", $town->getName());
+    }
+
+    /**
+     * Создает экземпляр класса GeoClient с мокнутым HttpClient
+     * @param string $jsonResponse
+     * @param int $httpStatusCode
+     * @return GeoClient
+     */
+    private function getGeoClientWithMockedHttpClient(string $jsonResponse, int $httpStatusCode): GeoClient
+    {
+        $httpClientMock = $this->createMock(Client::class);
+        $httpClientResponseMock = $this->createMock(Response::class);
+        $httpClientResponseMock->method('getBody')->willReturn($jsonResponse);
+        $httpClientResponseMock->method('getStatusCode')->willReturn($httpStatusCode);
+        $httpClientMock->method('request')->willReturn($httpClientResponseMock);
+
+        $geoClient = new GeoClient('http://test.local', 'my_token');
+        $geoClient->setHttpClient($httpClientMock);
+
+        return $geoClient;
     }
 }
